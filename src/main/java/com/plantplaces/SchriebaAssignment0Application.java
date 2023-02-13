55ab342253a485c047b2069c2f413a2e1a9ea5f5
schriewer11 <schriewer11@yahoo.com>
2023-02-12 19:45:39 -0500

    initial commit of spring boot project

---
+++ src/main/java/com/plantplaces/SchriebaAssignment0Application.java
@@ -0,0 +1,13 @@
+package com.plantplaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchriebaAssignment0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchriebaAssignment0Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/plantplaces/schrieba_assignment0/SchriebaAssignment0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plantplaces.schrieba_assignment0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchriebaAssignment0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
