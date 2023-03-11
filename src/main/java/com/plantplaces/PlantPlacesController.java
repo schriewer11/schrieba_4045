97002c9326375337da8f154b06d804e70821a80a
schriewer11 <schriewer11@yahoo.com>
2023-03-11 18:59:12 -0500

    Added comment

--- src/main/java/com/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -5,7 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PlantPlacesController {
-
+	
+	// commit test
 	@RequestMapping("/start")
 	public String start() {
 		return "start";
